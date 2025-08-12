84594abebabc0c381b819812a2fa9107e5702580
vinayak-2003 <vinayakkanchan03@gmail.com>
2025-08-12 21:49:54 +0530

    improved logging process by removing the print statements

--- backend/src/utils/logging_func.py
+++ backend/src/utils/logging_func.py
@@ -16,13 +16,12 @@ logger = logging.getLogger(__name__)
 def log_call_event(call_sid: str, event: str, details: dict = None, level: str = "info"):
     """
     A structured logger for call events.
-    
+
     Args:
-        call_sid (string): unique ID for a specific call when there is a call for the webhook endpoint from twilio
-        event (string): short and machine-readable name for what event just happened
-        details (dictionary): data payload for an event, stores actual information
-        level (string): logging level like info, error
-        
+        call_sid (str): Unique ID for a specific call when there is a call to the webhook endpoint from Twilio.
+        event (str): Short and machine-readable name for what event just happened.
+        details (dict, optional): Data payload for the event, containing actual information.
+        level (str, optional): Logging level such as "info", "error", "debug". Defaults to "info".
     """
     message = f"CALL_EVENT: [SID: {call_sid}] [Event: {event}]"
     if details:
